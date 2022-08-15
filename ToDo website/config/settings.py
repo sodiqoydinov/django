f4c123073547735ae83f78db94229a692bd80bac
Sodiq <sodiq7898@gmail.com>
2022-08-15 18:28:24 +0500

    Todo website changed

--- ToDo website/config/settings.py
+++ ToDo website/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3s6bd!k*mydnwi$99t3v!o2wcdk_rx*hhvs&5%-+4yqlj@_3_c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
