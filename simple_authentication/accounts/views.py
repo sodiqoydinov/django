e0afbf873742000f800863bb7d8b5f818c25709d
Sodiq <sodiq7898@gmail.com>
2022-08-18 19:55:37 +0500

    Simple authentication system
    Simple authentication system in django3 for beginners

---
+++ simple_authentication/accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+def home(request):
+	return render(request, 'auth/base.html')
+
+def register(request):
+	if request.method=='POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			return redirect('login')
+	else:
+		form = UserCreationForm()
+	context = {'form': form}
+	return render(request, 'registration/signup.html', context)
